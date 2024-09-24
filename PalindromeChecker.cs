1e9a12d4aabfc1e1e6f6803bd99beaf5143dbd43
Durgesh Beldar <138119506+Durgeshbeldar@users.noreply.github.com>
2024-09-24 21:13:55 +0530

    Update PalindromeChecker.cs

--- PalindromeChecker.cs
+++ PalindromeChecker.cs
@@ -13,12 +13,11 @@ class PalindromeChecker {
   // IsPalindrome Function is used to check the number is Palindrome OR Not.
   public static bool IsPalindrome(int number) {
     const int deciDevider = 10;
-    const int Zero = 0;
     // Negative numbers are not palindromes
-    if (number < Zero) return false;
+    if (number < 0) return false;
     int originalNumber = number;
-    int reverseNumber = Zero;
-    while (number > Zero) {
+    int reverseNumber = 0;
+    while (number > 0) {
       // Extracting the digit from back
       int remainder = number % deciDevider;
       //Adding the Extracted digit to get reverse of originalNumber
